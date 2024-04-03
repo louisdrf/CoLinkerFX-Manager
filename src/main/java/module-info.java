77008bd38f52a05f67b3635324cda429db1822cd
Louis <louis.driffort@gmail.com>
2024-04-03 23:02:41 +0200

    init project with JFX

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.colinker.colinkerjfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.colinker.colinkerjfx to javafx.fxml;
+    exports com.colinker.colinkerjfx;
+}
