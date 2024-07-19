ea205f7a272cf7c1528798b73fa72ba1e438ec5f
Louis <louis.driffort@gmail.com>
2024-07-19 15:28:11 +0200

    add navbar logout event

--- src/main/java/com/colinker/controllers/NavbarController.java
+++ src/main/java/com/colinker/controllers/NavbarController.java
@@ -1,11 +1,27 @@
 package com.colinker.controllers;
 
 import com.colinker.helpers.SceneRouter;
+import com.colinker.events.EventBus;
+import com.colinker.events.PluginLoadedEvent;
+import com.colinker.plugins.Plugin;
+import com.colinker.services.UserPropertiesService;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
 
 import java.io.IOException;
+import java.util.List;
 
 public class NavbarController {
+
+    public void initialize() {
+        EventBus.addEventHandler(PluginLoadedEvent.PLUGIN_LOADED, event -> {
+            List<Plugin> loadedPlugins = ((PluginLoadedEvent) event).getPlugins();
+            System.out.println(loadedPlugins);
+        });
+    }
 
     public void showCalendarPage(MouseEvent mouseEvent) throws IOException {
         SceneRouter.showCalendarPage();
@@ -25,5 +41,10 @@ public class NavbarController {
 
     public void showPluginsPage(MouseEvent mouseEvent) throws IOException {
         SceneRouter.showPluginsPage();
+    }
+
+    public void logout(MouseEvent mouseEvent) throws IOException {
+        UserPropertiesService.cleanProperties();
+        SceneRouter.showLoginPage();
     }
 }
