0632fd9067e5e8f690b4502501298ad1412ec611
goodfoodtruck <mielatomique@protonmail.com>
2024-07-22 18:01:12 +0200

    fix plugin notes

--- src/main/java/com/colinker/helpers/SceneRouter.java
+++ src/main/java/com/colinker/helpers/SceneRouter.java
@@ -1,10 +1,12 @@
 package com.colinker.helpers;
 
 import com.colinker.App;
+import com.colinker.plugins.PluginLoader;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class SceneRouter {
@@ -40,10 +42,20 @@ public class SceneRouter {
         stage.show();
     }
 
-    public static void showNotesPage() throws IOException {
-        currentScene = loadScene("notes/note.fxml");
-        stage.setScene(currentScene);
-        stage.show();
+    public static void showNotesPage() {
+        FXMLLoader loader = new FXMLLoader(App.class.getResource("notes/note.fxml"));
+        try {
+            PluginLoader pluginLoader = PluginLoader.getInstance();
+            Class<?> controllerClass = pluginLoader.loadClass(new File("com/colinker/plugins/notes/NotesController.class"));
+
+            Object controller = controllerClass.getDeclaredConstructor().newInstance();
+            loader.setController(controller);
+            currentScene = new Scene(loader.load(), 1280, 720);
+            stage.setScene(currentScene);
+            stage.show();
+        } catch (IOException | ReflectiveOperationException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void showPluginsPage() throws IOException {
@@ -54,7 +66,6 @@ public class SceneRouter {
 
     private static Scene loadScene(String fxmlFilePath) throws IOException {
         FXMLLoader loader = new FXMLLoader(App.class.getResource(fxmlFilePath));
-        // TODO gérer le cas où il n'y a pas le fichier
         return new Scene(loader.load(), 1280, 720);
     }
 }
